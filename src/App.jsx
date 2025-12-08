806eeb9c82ed08f6840d26bb317b9adff186846b
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-08 12:15:59 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App(){
     try { return JSON.parse(localStorage.getItem('stx_addresses')||'[]') } catch(e){ return [] }
   })
   const [price, setPrice] = useState(null)
-  const [user, setUser] = useState(getUserData())
+  const [user, stUser] = useState(getUserData())
 
   useEffect(()=>{
     async function loadPrice(){ setPrice(await getPriceUSD()) }
