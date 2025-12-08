62a819c6ea092df05a8cfb066433ada93c4e3186
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-08 07:46:33 +0100

    Update popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -8,7 +8,7 @@ document.getElementById'go').addEventListener('click', async ()=>{
   tr
     const res = await fetch(`${API}/v2/accounts/${a}
     if(!res.ok) throw new Error('Fetch failed ' + res.status)
-    const j = await res.json()
+    const j await res.json()
     const bal = Number(j.balance||0)/1_000_000
     out.innerHTML = `<div class="bal">Balance: ${bal.toFixed(6)} STX</div>`
   }catch(e){
