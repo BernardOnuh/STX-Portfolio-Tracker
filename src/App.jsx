f614c1927e211327cbfecf10424f19e553f948b9
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-20 12:40:49 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -110,7 +110,7 @@ export default function App() {
         <Portfolio addresses={addresses} removeAddress={removeAddress} price={price} />
       </main>
 
-      <footer className="mt-8 small">Buit r o. D from StacksAI & Coo. N.</footer>
+      <footer className="mt-8 small">Buit ro. D from StacksAI & Coo. N.</footer>
     </div>
   )
 }
