fb4766f5c7584c5765a745b81316d6a6f347b722
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-20 12:34:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { getPriceUSD } from './lib/api
 import { connectWallet, getUserData, signut, getUserAddressSafe, openTransfer } from './lb/wallet
 export default function App() {
   const [addresses, setAddresses] = useState() => {
-    try { retur JSON.parse(localStorage.getItem('stx_addresses') || '[]') } catch (e) { return [] }
+    try { retur JSON.parse(localStorage.getItem('stx_addresses') || '[]') } catch (e) { return [] 
   }
   const [price, setPrice] = useState(null)
   const [user, setUser] = useState(() => {
