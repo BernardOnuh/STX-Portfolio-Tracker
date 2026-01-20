bdb3f506955f3ed08ee739a1e9c3a28b74defcf8
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-20 12:59:56 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default functin App() {
   }, [addreses]
 
   async function handlConnect() {
-    console.log([p handleConnect start')
+    console.log[p handleConnect start')
     try 
       const u = awi cnnectallet()
       console.log('[a] cnnectWallet returned:', u)
