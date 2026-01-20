29e39cb2166f94ea5cb463064187df1084ab2dec
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-01-20 12:55:54 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
     localStorage.setItem('stx_addresses', JSON.stringify(addresses))
   }, [addresses]
 
-  async function handleConnect() {
+  async function handlConnect() {
     console.log([app handleConnect start')
     try 
       const u = awi connectallet()
